c54468d1647692ad2bfe0d7ae0bfc021c8a03b04
Ian Vitor Moraes da Silva <76691828+IanVitor@users.noreply.github.com>
2021-10-18 20:45:18 -0300

    Update program.c

--- program.c
+++ program.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
-int main(){
-  printf("Hello, world!");
-  return 0;
+void main(){
+	//Declaração de variáveis
+	int mat[3][3];
+	int Maior, Menor, C, J;
+	//Laço para preencher a matriz
+	for(C=0;C<3;C++)
+	{
+		for(J=0;J<3;J++)
+		{
+			printf("linha %i coluna %i:", C+1, J+1);
+			scanf("%i", &mat[C][J]);
+		}
+			printf("\n");
+	}
+	//Gambiarra para inicializar as variáveis
+	Menor = mat[0][0];
+	Maior = mat[0][0];
+	//Laço para comparar as variáveis e distribui-lás 
+	for(C=0;C<3;C++)
+		for(J=0;J<3;J++)
+		{
+			if(mat[C][J] > Maior)
+			{
+				Maior = mat[C] [J];
+			}
+			else
+			{
+				if(mat[C][J]< Menor)
+				{
+					Menor = mat[C][J];
+				}
+			}
+		}
+	printf("Maior = %i\nMenor = %i", Maior, Menor);
 }
